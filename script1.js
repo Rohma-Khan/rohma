ea707442988f406d3cc35dd49bb997c5fb2fefd2
Rohma-Khan <53379843+Rohma-Khan@users.noreply.github.com>
2021-08-04 01:04:53 -0400

    Putting this all on git now

---
+++ script1.js
@@ -0,0 +1,17 @@
+var value;
+var hover = document.getElementsByClassName('hexagon');
+
+for (var i = 0; i < hover.length; i++) {
+  hover[i].onmouseover = mouseOver;
+//   hover[i].onmouseout = mouseOut;
+}
+
+// onmouseover="mouseOver()" onmouseout="setTimeout(mouseOut, 1000)"
+function mouseOver() {
+    this.style.backgroundColor ='#837C93';
+  }
+
+// function mouseOut() {
+//     // this.style.visibility='hidden';
+//     this.style.backgroundColor ='#ffffff'
+//   }
